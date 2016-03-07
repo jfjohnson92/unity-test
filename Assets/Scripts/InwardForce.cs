c7b010f3af27c64ea193104874a4a9281e073bf4
Jeff Johnson <jfjohnson@solstice-consulting.com>
2016-03-07 10:39:49 -0600

    initial commit

---
+++ Assets/Scripts/InwardForce.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class InwardForce : MonoBehaviour {
+
+	public float attractionForce = 6f;
+
+	Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		body.AddForce (transform.position * -attractionForce);
+	}
+}
